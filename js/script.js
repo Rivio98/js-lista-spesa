8598fc60219690bb9e9a8dd689055293858d17db
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-02 14:53:40 +0200

    [UPD] Added const to recover list items

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 //inizializzo l'array
-
 const prodotti = ['pollo', 'pasta', 'riso', 'pomodori', 'pesce', 'uova', 'gallette', 'olio', 'insalata'];
+
+//recupero l'elemento che deve contenere la mia lista
+const lista = document.getElementById('lista');
